f03f99573fdb07bd616ec756a5805b9c92046fb8
Borja Luengo Prieto <borja.luengoprieto@affirm.com>
2023-08-25 18:19:17 +0200

    Move commit review logic out of main function

--- audit-export.py
+++ audit-export.py
@@ -12,16 +12,16 @@ from time import sleep
 def create_logger(name:str):
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s: %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
-    
+
     # Use stdout as the stream to write log messages to
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(formatter)
-    
+
     # Create logger with level INFO
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
-    
+
     return logger
 
 def get_filename(file_name:str):
@@ -33,17 +33,80 @@ def get_filename(file_name:str):
         return ".".join(segments)
     else:
         return file_name
-    
-def write_commit_to_file(fp, repository, commit, reviews):
+
+def write_commit_to_file(logger, fp, repository, commit, reviews):
     commit_detail = commit.commit
     detail=f'{commit_detail.author.date},{reviews},{commit.sha},{commit_detail.author.name},{commit_detail.author.email},https://github.com/{repository}/commit/{commit.sha}\n'
-    print(detail)
+    logger.info(detail)
     writeout(fp, detail)
     fp.flush()
 
-def writeout(fp, content:str):
+def writeout(logger, fp, content:str):
     fp.write(content)
-    print(content)
+    logger.info(content)
+
+def commit_report(logger, fp, github_api, repository, branch, required_reviews, startDate, endDate, csv_file, file_name):
+    # Print headers
+    fp.write("Commit Date,No. Reviews,Commit,Author,Email,Commit link\n")
+
+    index=0
+
+    try:
+        repo = github_api.get_repo(repository)
+    except Exception as e:
+        logger.fatal(f"Unable to retreive repo {repository} info: {e}")
+
+    try:
+        commits = repo.get_commits(branch, "", since=startDate, until=endDate)
+    except Exception as e:
+        logger.fatal(f"Unable to get commits from repo {repository}: {e}")
+
+    for commit in commits:
+        while True:
+            try:
+                pull_requests = commit.get_pulls()
+
+                if pull_requests.totalCount == 0:
+                    logger.warning(f'no commits, adding {commit.sha}')
+                    write_commit_to_file(logger, fp, repository, commit, 0)
+                    fp.flush()
+                    index+=1
+                else:
+                    for pull_request in pull_requests:
+                        while True:
+                            try:
+                                reviews = pull_request.get_reviews()
+                                if reviews.totalCount < required_reviews:
+                                    commit_detail = commit.commit
+                                    write_commit_to_file(fp, repository, commit, reviews.totalCount)
+                                    index+=1
+                                break
+                            except RateLimitExceededException:
+                                print(f'{commit.sha} exceeded rate limit waiting')
+                                sleep(3600/4)
+                                continue
+                            except Exception as e:
+                                print(f'{commit.sha} unknown exception occurred')
+                                print(f'{commit.sha} {e.args}')
+                                sleep(3600/4)
+                                continue
+                break
+            except Exception as e:
+                print(f"An exception occurred: {e}")
+                sleep(3600/4)
+                continue
+    fp.close()
+
+    file_checksum = hashlib.md5(open(csv_file,'rb').read()).hexdigest()
+    checksum_filename = f'{file_name}.hash'
+    fp = open(checksum_filename, "w", buffering=1)
+
+    writeout(logger, fp, f"Checksum: {file_checksum}\n")
+    writeout(logger, fp, f"Records: {index}\n")
+    writeout(logger, fp, f"Date: {datetime.now().strftime('%Y-%m-%d')}\n")
+
+    fp.close()
+
 
 def main():
     # Create logger
@@ -77,7 +140,7 @@ def main():
                         help="Number of weeks to go back",
                         type=int,
                         default=12)
-    
+
     args = parser.parse_args()
 
     # Format repo name
@@ -110,67 +173,16 @@ def main():
     csv_file = f"{file_name}.csv"
     fp = open(csv_file, "w", buffering=1)
 
-    # Print headers
-    fp.write("Commit Date,No. Reviews,Commit,Author,Email,Commit link\n")
-    
-    # 
-    index=0
-   
-    try:
-        repo = github_api.get_repo(repository)
-    except Exception as e:
-        logger.fatal(f"Unable to retreive repo {repository} info: {e}")
-    
-    try:
-        commits = repo.get_commits(args.branch, "", since=startDate, until=endDate)
-    except Exception as e:
-        logger.fatal(f"Unable to get commits from repo {repository}: {e}")
-
-    for commit in commits:
-        while True:
-            try:
-                pull_requests = commit.get_pulls()
-
-                if pull_requests.totalCount == 0:
-                    print(f'no commits, adding {commit.sha}')
-                    write_commit_to_file(fp, repository, commit, 0)
-                    fp.flush()
-                    index+=1
-                else:
-                    for pull_request in pull_requests:
-                        while True:
-                            try:
-                                reviews = pull_request.get_reviews()
-                                if reviews.totalCount < args.required_review_num:
-                                    commit_detail = commit.commit
-                                    write_commit_to_file(fp, repository, commit, reviews.totalCount)
-                                    index+=1
-                                break
-                            except RateLimitExceededException:
-                                print(f'{commit.sha} exceeded rate limit waiting')
-                                sleep(3600/4)
-                                continue
-                            except Exception as e:
-                                print(f'{commit.sha} unknown exception occurred')
-                                print(f'{commit.sha} {e.args}')
-                                sleep(3600/4)
-                                continue
-                break
-            except Exception as e:
-                print(f"An exception occurred: {e}")
-                sleep(3600/4)
-                continue
-    fp.close()
-
-    file_checksum = hashlib.md5(open(csv_file,'rb').read()).hexdigest()
-    checksum_filename = f'{file_name}.hash'
-    fp = open(checksum_filename, "w", buffering=1)
-
-    writeout(fp,f"Checksum: {file_checksum}\n")
-    writeout(fp,f"Records: {index}\n")
-    writeout(fp,f"Date: {datetime.now().strftime('%Y-%m-%d')}\n")
-
-    fp.close()
+    commit_report(logger,
+                  fp,
+                  github_api,
+                  repository,
+                  args.branch,
+                  args.required_review_num,
+                  startDate,
+                  endDate,
+                  csv_file,
+                  file_name)
 
 
 if __name__ == "__main__":
